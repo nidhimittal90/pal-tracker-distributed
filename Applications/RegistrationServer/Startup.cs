88b566a3b3309829fa0ebb32c89cb37863cce97b
=nidhimittal90 <=nidhimittal1990@gmail.com>
2019-10-24 14:58:47 +0530

    circuit breakers

--- Applications/RegistrationServer/Startup.cs
+++ Applications/RegistrationServer/Startup.cs
@@ -35,6 +35,7 @@ namespace RegistrationServer
             services.AddScoped<IUserDataGateway, UserDataGateway>();
             services.AddScoped<IRegistrationService, RegistrationService>();
             services.AddDiscoveryClient(Configuration);
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
